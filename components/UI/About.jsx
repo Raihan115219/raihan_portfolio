aba22f3e75fcc559bdf695c62ccb1b32ce7cabfa
Raihan115219 <aburaihan115219@gmail.com>
2023-03-14 22:08:11 +0600

    first commit

---
+++ components/UI/About.jsx
@@ -0,0 +1,184 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import Link from "next/link";
+import SectionSubtitle from "./SectionSubtitle";
+import Image from "next/image";
+import img01 from "../../public/images/img-01.jpg";
+import img02 from "../../public/images/img-02.jpg";
+import img03 from "../../public/images/img-03.jpg";
+import img04 from "../../public/images/hero.jpg";
+import profile2 from "../../public/images/porf-2.jpg";
+import classes from "../../styles/about.module.css";
+
+const About = () => {
+  return (
+    <section id="about">
+      <Container>
+        <Row>
+          <Col lg="6" className={`${classes.about__content}`}>
+            <SectionSubtitle subtitle="About me" />
+            <h3 className="mt-4">I&apos;m here</h3>
+            <h3 className="mb-4">to help your next project</h3>
+            <p>
+              Developing and maintaining web applications using React.js and
+              other related technologies. Collaborating with cross-functional
+              teams including designers, product managers, and other developers
+              to create high-quality products. Implementing responsive design
+              and ensuring cross-browser compatibility.Participating in code
+              reviews and providing constructive feedback to other developers.
+            </p>
+
+            <div className=" d-flex align-items-center gap-5">
+              <div>
+                <h6 className=" d-flex align-items-center gap-2 mt-3 fw-normal">
+                  <span className={`${classes.about__icon}`}>
+                    <i className="ri-checkbox-circle-line"></i>
+                  </span>
+                  Html-5
+                </h6>
+                <h6 className=" d-flex align-items-center gap-2 mt-3 fw-normal">
+                  <span className={`${classes.about__icon}`}>
+                    <i className="ri-checkbox-circle-line"></i>
+                  </span>
+                  Css-3
+                </h6>
+                <h6 className=" d-flex align-items-center gap-2 mt-3 fw-normal">
+                  <span className={`${classes.about__icon}`}>
+                    <i className="ri-checkbox-circle-line"></i>
+                  </span>
+                  Bootstrap
+                </h6>
+                <h6 className=" d-flex align-items-center gap-2 mt-3 fw-normal">
+                  <span className={`${classes.about__icon}`}>
+                    <i className="ri-checkbox-circle-line"></i>
+                  </span>
+                  Tailwind CSS
+                </h6>
+
+                <h6 className=" d-flex align-items-center gap-2 mt-3 fw-normal">
+                  <span className={`${classes.about__icon}`}>
+                    <i className="ri-checkbox-circle-line"></i>
+                  </span>
+                  Chakra UI
+                </h6>
+              </div>
+
+              <div>
+                <h6 className=" d-flex align-items-center gap-2 mt-3 fw-normal">
+                  <span className={`${classes.about__icon}`}>
+                    <i className="ri-checkbox-circle-line"></i>
+                  </span>
+                  JavaScript
+                </h6>
+                <h6 className=" d-flex align-items-center gap-2 mt-3 fw-normal">
+                  <span className={`${classes.about__icon}`}>
+                    <i className="ri-checkbox-circle-line"></i>
+                  </span>
+                  TypeScript
+                </h6>
+                <h6 className=" d-flex align-items-center gap-2 mt-3 fw-normal">
+                  <span className={`${classes.about__icon}`}>
+                    <i className="ri-checkbox-circle-line"></i>
+                  </span>
+                  React JS
+                </h6>
+                <h6 className=" d-flex align-items-center gap-2 mt-3 fw-normal">
+                  <span className={`${classes.about__icon}`}>
+                    <i className="ri-checkbox-circle-line"></i>
+                  </span>
+                  Next JS
+                </h6>
+
+                <h6 className=" d-flex align-items-center gap-2 mt-3 fw-normal">
+                  <span className={`${classes.about__icon}`}>
+                    <i className="ri-checkbox-circle-line"></i>
+                  </span>
+                  Node JS
+                </h6>
+              </div>
+              <div>
+                <h6 className=" d-flex align-items-center gap-2 mt-3 fw-normal">
+                  <span className={`${classes.about__icon}`}>
+                    <i className="ri-checkbox-circle-line"></i>
+                  </span>
+                  Express JS
+                </h6>
+                <h6 className=" d-flex align-items-center gap-2 mt-3 fw-normal">
+                  <span className={`${classes.about__icon}`}>
+                    <i className="ri-checkbox-circle-line"></i>
+                  </span>
+                  MongoDB
+                </h6>
+                <h6 className=" d-flex align-items-center gap-2 mt-3 fw-normal">
+                  <span className={`${classes.about__icon}`}>
+                    <i className="ri-checkbox-circle-line"></i>
+                  </span>
+                  Rest API
+                </h6>
+                <h6 className=" d-flex align-items-center gap-2 mt-3 fw-normal">
+                  <span className={`${classes.about__icon}`}>
+                    <i className="ri-checkbox-circle-line"></i>
+                  </span>
+                  GraphQL(query & Mutation)
+                </h6>
+
+                <h6 className=" d-flex align-items-center gap-2 mt-3 fw-normal">
+                  <span className={`${classes.about__icon}`}>
+                    <i className="ri-checkbox-circle-line"></i>
+                  </span>
+                  Git || Bit-bucket || JIRA
+                </h6>
+              </div>
+            </div>
+
+            <div className="mt-5">
+              <button className="primary__btn">
+                <Link href="#portfolio">My Portfolio</Link>
+              </button>
+
+              <button className="secondary__btn">
+                <Link href="#">Download CV</Link>
+              </button>
+            </div>
+          </Col>
+
+          <Col lg="6">
+            <div
+              className={`${classes.about__img__gallery} d-flex gap-4 justify-content-end`}
+            >
+              <div className=" d-flex flex-column mb-3">
+                <div
+                  className={`${classes.about__img} ${classes.about__img__box}`}
+                >
+                  <Image src={img01} alt="about-img" />
+                </div>
+
+                <div
+                  className={`${classes.about__img} ${classes.about__img__box}`}
+                >
+                  <Image src={profile2} alt="about-img" />
+                </div>
+              </div>
+
+              <div className=" d-flex flex-column mb-3">
+                <div
+                  className={`${classes.about__img} ${classes.about__img__box}`}
+                >
+                  <Image src={img03} alt="about-img" />
+                </div>
+
+                <div
+                  className={`${classes.about__img} ${classes.about__img__box}`}
+                >
+                  <Image src={img04} alt="about-img" />
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default About;
